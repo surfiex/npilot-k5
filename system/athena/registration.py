a8b56d60e7dd56f1bef80977a96250c78826d3c0
surfiex <lsj1529@gmail.com>
2025-11-15 12:14:55 +0900

    c3xlite-fix

--- system/athena/registration.py
+++ system/athena/registration.py
@@ -16,6 +16,9 @@ from openpilot.common.swaglog import cloudlog
 
 UNREGISTERED_DONGLE_ID = "UnregisteredDevice"
 
+DUMMY_IMEI1 = '865420071781912'
+DUMMY_IMEI2 = '865420071781904'
+
 def is_registered_device() -> bool:
   dongle = Params().get("DongleId")
   return dongle not in (None, UNREGISTERED_DONGLE_ID)
@@ -32,7 +35,7 @@ def register(show_spinner=False) -> str | None:
   entirely.
   """
   params = Params()
-  return UNREGISTERED_DONGLE_ID
+  #return UNREGISTERED_DONGLE_ID
   dongle_id: str | None = params.get("DongleId")
   if dongle_id is None and Path(Paths.persist_root()+"/comma/dongle_id").is_file():
     # not all devices will have this; added early in comma 3X production (2/28/24)
